12b361776f45423a6d63ed8c72a636a82ed3b02f
Grodrigues34 <grodrigues2649@gmail.com>
2026-07-08 17:24:05 -0300

    Feat: implement AI simple memory feature 	*Create method in ChatMessageRepo to find last 10 messages 	*Refactor AiService to send last 10 messages in user prompt 	*Create get endpoint to receive last 10 messages

--- aurora-backend/src/main/java/com/github/gr/aurora_bookstore/controller/AiController.java
+++ aurora-backend/src/main/java/com/github/gr/aurora_bookstore/controller/AiController.java
@@ -1,14 +1,19 @@
 package com.github.gr.aurora_bookstore.controller;
 
 import com.github.gr.aurora_bookstore.dto.aiDto.ChatRequest;
+import com.github.gr.aurora_bookstore.dto.chatMessageDto.ChatMessageReadDto;
 import com.github.gr.aurora_bookstore.service.AiService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -23,6 +28,11 @@ public class AiController {
         return ResponseEntity.ok()
                 .contentType(MediaType.TEXT_EVENT_STREAM)
                 .body(flux);
+    }
+
+    @GetMapping("/api/chat/history/{userId}")
+    public ResponseEntity<List<ChatMessageReadDto>> getChatHistory(@PathVariable Long userId) {
+        return ResponseEntity.ok(aiService.chatMessagesToDto(userId));
     }
 
 }
--- aurora-backend/src/main/java/com/github/gr/aurora_bookstore/service/AiService.java
+++ aurora-backend/src/main/java/com/github/gr/aurora_bookstore/service/AiService.java
@@ -1,12 +1,20 @@
 package com.github.gr.aurora_bookstore.service;
 
 import com.github.gr.aurora_bookstore.dto.aiDto.ChatRequest;
+import com.github.gr.aurora_bookstore.dto.chatMessageDto.ChatMessageReadDto;
 import com.github.gr.aurora_bookstore.model.entity.ChatMessage;
 import com.github.gr.aurora_bookstore.model.entity.User;
+import com.github.gr.aurora_bookstore.model.mapper.ChatMessageMapper;
 import com.github.gr.aurora_bookstore.repository.ChatMessageRepository;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class AiService {
@@ -22,8 +30,8 @@ public class AiService {
         this.userService = userService;
     }
 
-    public Flux<String> processUserMessage(String message) {
-        return chatClient.prompt().user(message).stream().content();
+    public Flux<String> processUserMessage(List<Message> messages) {
+        return chatClient.prompt().messages(messages).stream().content();
     }
 
     public void saveChatMessage(String message, Long userId, String chatRole) {
@@ -36,15 +44,40 @@ public class AiService {
         chatMessageRepository.save(newChatMessage);
     }
 
+    public List<Message> getSpringAiHistory(Long userId) {
+        List<ChatMessage> lastMessages = chatMessageRepository.findTop10ByUserIdOrderByCreatedAtDesc(userId).reversed();
+        List<Message> springAiMessages = new ArrayList<>();
+
+        for (ChatMessage chatMessage : lastMessages) {
+            if ("user".equalsIgnoreCase(chatMessage.getChatRole())) {
+                springAiMessages.add(new UserMessage(chatMessage.getContent()));
+            } else {
+                springAiMessages.add(new AssistantMessage(chatMessage.getContent()));
+            }
+        }
+        return springAiMessages;
+    }
+
+    public List<ChatMessageReadDto> chatMessagesToDto(Long userId) {
+        List<ChatMessage> lastMessages = chatMessageRepository.findTop10ByUserIdOrderByCreatedAtDesc(userId).reversed();
+        List<ChatMessageReadDto> chatMessageReadDtos = new ArrayList<>();
+        for (ChatMessage chatMessage : lastMessages) {
+            chatMessageReadDtos.add(ChatMessageMapper.toReadDto(chatMessage));
+        }
+        return chatMessageReadDtos;
+    }
+
     public Flux<String> receiveUserMessage(ChatRequest chatRequest) {
         String userMessage = chatRequest.message();
         Long userId = Long.valueOf(chatRequest.userId());
 
         saveChatMessage(userMessage, userId, "user");
 
+        List<Message> history = getSpringAiHistory(userId);
+
         StringBuilder fullAiResponse = new StringBuilder();
 
-        return processUserMessage(userMessage)
+        return processUserMessage(history)
                 .doOnNext(chunk -> fullAiResponse.append(chunk))
                 .doOnComplete(() -> {
                     saveChatMessage(fullAiResponse.toString(), userId, "ai");
