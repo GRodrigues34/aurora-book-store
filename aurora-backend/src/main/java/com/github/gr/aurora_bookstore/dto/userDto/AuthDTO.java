25666777d892b0aba133f919f482566aedea9521
Grodrigues34 <grodrigues2649@gmail.com>
2026-07-13 14:46:00 -0300

    Feat: Add initial Security config components

---
+++ aurora-backend/src/main/java/com/github/gr/aurora_bookstore/dto/userDto/AuthDTO.java
@@ -0,0 +1,4 @@
+package com.github.gr.aurora_bookstore.dto.userDto;
+
+public class AuthDTO {
+}
--- aurora-backend/src/main/java/com/github/gr/aurora_bookstore/model/entity/User.java
+++ aurora-backend/src/main/java/com/github/gr/aurora_bookstore/model/entity/User.java
@@ -1,12 +1,17 @@
 package com.github.gr.aurora_bookstore.model.entity;
 
+import com.github.gr.aurora_bookstore.model.enums.UserRole;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.List;
 
 @Data
@@ -15,7 +20,7 @@ import java.util.List;
 @AllArgsConstructor
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,11 +36,39 @@ public class User {
     private String password;
 
     @Column(nullable = false)
-    private String role;
+    private UserRole role;
 
     @Column(name = "created_at", insertable = false, updatable = false)
     private LocalDateTime createdAt;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ChatMessage> chatMessages;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if(this.role == UserRole.ADMIN){
+            return List.of( new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        }
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
